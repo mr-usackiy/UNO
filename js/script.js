438fc7136dd916506aa59ef811f50d6ec23ad855
mr-usackiy <usackiy95@gmail.com>
2021-06-07 13:34:54 +0300

    07.06.21 13.34

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
-document.querySelector('.search-form_submit').addEventListener('click', function() {
+document.querySelector('.arrow_img').addEventListener('click', function() {
   offset = offset + 290;
   sliderLine.style.left= -offset + 'px';
   if (offset > 1450)
   offset = -290;
 });
 
-document.querySelector('.search-form_down').addEventListener('click', function() {
+document.querySelector('.arrow_img1').addEventListener('click', function() {
     offset = offset -290;
     sliderLine.style.left= -offset +'px';
     if (offset < -1450)
